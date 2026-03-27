605e4d5479a641721f4bd5c399834fa3c0b5b80a
gaby.jorge2020@gmail.com <gaby.jorge2020@gmail.com>
2026-03-27 11:34:23 +0000

    fixed error in stg_sales query, missed a comma

--- models/example/Staging/stg_sales.sql
+++ models/example/Staging/stg_sales.sql
@@ -3,6 +3,6 @@ SELECT
     cast(customers_id AS int64) AS customers_id,
     cast(orders_id AS int64) AS orders_id,
     cast(products_id AS int64) AS products_id,
-    cast(replace(net_sales, ',', '.') AS float64) AS net_sales
+    cast(replace(net_sales, ',', '.') AS float64) AS net_sales,
     cast(qty AS int64) AS qt
 FROM `raw_data.sales_raw`

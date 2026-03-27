853c18c7a1bf891321ad9ecb70d2e27bde59aa6e
gaby.jorge2020@gmail.com <gaby.jorge2020@gmail.com>
2026-03-27 12:14:00 +0000

    - stg querys created by orders and sales. - deleted my_first_dbt_model.sql and my_second_dbt_model.sql

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -3,4 +3,4 @@ SELECT
     cast(customers_id AS int64) AS customers_id,
     cast(orders_id AS int64) AS orders_id,
     cast(replace(net_sales, ',', '.') AS float64) AS net_sales
-FROM `raw_data.orders_raw`
+FROM `raw_data.raw_orders`
--- models/Staging/stg_sales.sql
+++ models/Staging/stg_sales.sql
@@ -1,8 +1,8 @@
 SELECT
     parse_date('%Y-%m-%d', date_date) AS date_date,
-    cast(customers_id AS int64) AS customers_id,
-    cast(orders_id AS int64) AS orders_id,
+    cast(customer_id AS int64) AS customer_id,
+    cast(order_id AS int64) AS order_id,
     cast(products_id AS int64) AS products_id,
     cast(replace(net_sales, ',', '.') AS float64) AS net_sales,
-    cast(qty AS int64) AS qt
-FROM `raw_data.sales_raw`
+    cast(qty AS int64) AS qty
+FROM `raw_data.raw_sales`

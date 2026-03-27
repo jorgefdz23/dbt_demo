8558de7a54383e536fdda78e11ea0b82bfb42f2e
gaby.jorge2020@gmail.com <gaby.jorge2020@gmail.com>
2026-03-27 13:32:29 +0000

    - stg_stragy.yml created with test for certain columns - recreated stg_astrafy__raw_orders with generate model for source yml - recreated stg_astrafy__raw_sales with generate model for source yml

--- models/Staging/astrafy/stg_sales.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    parse_date('%Y-%m-%d', date_date) AS date_date,
-    cast(customer_id AS int64) AS customer_id,
-    cast(order_id AS int64) AS order_id,
-    cast(products_id AS int64) AS products_id,
-    cast(replace(net_sales, ',', '.') AS float64) AS net_sales,
-    cast(qty AS int64) AS qty
-FROM {{source('astrafy','raw_sales')}}

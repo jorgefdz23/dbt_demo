174f947323f3070ff710d92e1b6c63922eec16e5
gaby.jorge2020@gmail.com <gaby.jorge2020@gmail.com>
2026-03-27 11:22:50 +0000

    Added Staging folder with transformation to orders y sales raw tables

---
+++ models/example/Staging/stg_orders.sql
@@ -0,0 +1,6 @@
+SELECT
+    parse_date('%Y-%m-%d', date_date) AS date_date,
+    cast(customers_id AS int64) AS customers_id,
+    cast(orders_id AS int64) AS orders_id,
+    cast(replace(net_sales, ',', '.') AS float64) AS net_sales
+FROM `raw_data.orders_raw`
